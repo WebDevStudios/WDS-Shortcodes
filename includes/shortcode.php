cd875634170170e81c029b99297feebc4e4cbc2e
Justin Sternberg <justin@dsgnwrks.pro>
2016-05-29 20:05:45 -0400

    Add a json_decode_att method to get decoded json shortcode attributes.
    Leaving the original magic decoding in the att method, but previous
    version had a bug with quotations. It's better to allow explicit
    declaration of a json decode request.

--- includes/shortcode.php
+++ includes/shortcode.php
@@ -65,7 +65,7 @@ class WDS_Shortcode {
 	 */
 	public function att( $att, $default = null ) {
 		if ( isset( $this->atts[ $att ] ) && ! empty( $this->atts[ $att ] ) ) {
-			return $this->maybe_json( $this->atts[ $att ] );
+			return $this->maybe_json_decode( $this->atts[ $att ] );
 		}
 
 		return $default;
@@ -91,6 +91,27 @@ class WDS_Shortcode {
 	}
 
 	/**
+	 * Attribute-getter for converting json values with a default fallback option.
+	 *
+	 * By default, CMB2 fields in the shortcode button form  which store an array
+	 * of information will be stored to the shortcode with a modified json format.
+	 *
+	 * @since  1.0.1
+	 *
+	 * @param  string $att     Attribute key.
+	 * @param  mixed  $default Optional default value for this key if no value is found.
+	 *
+	 * @return mixed           Value for this attribute (or the default)
+	 */
+	public function json_decode_att( $att, $default = null ) {
+		if ( isset( $this->atts[ $att ] ) && ! empty( $this->atts[ $att ] ) ) {
+			return $this->maybe_json_decode( $this->atts[ $att ], 1 );
+		}
+
+		return $default;
+	}
+
+	/**
 	 * If a modified JSON value was stored as the attribute (Shortcode_Button handling)
 	 * then restore the JSON string, and json_decode it.
 	 *
@@ -100,8 +121,12 @@ class WDS_Shortcode {
 	 *
 	 * @return string         Possibly modified attribute value
 	 */
-	protected function maybe_json( $value ) {
-		if ( ! is_string( $value ) || 0 !== strpos( $value, '|~' ) ) {
+	protected function maybe_json_decode( $value, $force = false ) {
+		if (
+			! $value
+			|| ! is_string( $value )
+			|| ( ! $force && 0 !== strpos( $value, '|~' ) )
+		) {
 			return $value;
 		}
 
